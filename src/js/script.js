c3d4f99b381c3f68054827983a540f78fe752b89
Cody <cody.ko@gmail.com>
2015-02-27 20:18:18 -0800

    added random gif

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,8 @@
-console.log("script running");
+var endpoint = 'http://api.giphy.com/v1/gifs/random';
+var params = { api_key : 'dc6zaTOxFJmzC', tag : 'owned' };
+var str = $.param( params );
+var url = endpoint + '?' + str;
+
+$.getJSON( url, function( data ) {
+  $('.gif').attr('src', data.data.image_url);
+});
