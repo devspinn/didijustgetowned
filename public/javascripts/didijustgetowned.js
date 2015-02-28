96ea50d7560708c4f8407cd1fa1beaf8f295f46e
marcusmolchany <marcusmolchany@gmail.com>
2015-02-28 13:57:28 -0500

    Fixed gif not showing up in Chrome

--- public/javascripts/didijustgetowned.js
+++ public/javascripts/didijustgetowned.js
@@ -1,8 +1,12 @@
-var endpoint = 'http://api.giphy.com/v1/gifs/random';
-var params = { api_key : 'dc6zaTOxFJmzC', tag : 'owned' };
-var str = $.param( params );
-var url = endpoint + '?' + str;
+var main = function() {
+  var endpoint = 'https://api.giphy.com/v1/gifs/random';
+  var params = { api_key : 'dc6zaTOxFJmzC', tag : 'owned' };
+  var str = $.param( params );
+  var url = endpoint + '?' + str;
 
-$.getJSON( url, function( data ) {
-  $('.gif').attr('src', data.data.image_url);
-});
+  $.getJSON( url, function( data ) {
+    $('.gif').attr('src', data.data.image_url);
+  });
+};
+
+$(document).ready(main);
