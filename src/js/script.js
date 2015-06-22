9571a672f2ad1b30b8ddb6534b28dd79e9bdb7e1
dtown <devonstownsend@gmail.com>
2015-06-22 15:23:41 -0700

    Revert "not sure.."
    This reverts commit 0e2abf5819b6196b11d892d9c4a85c961d6c3541.

--- src/js/script.js
+++ src/js/script.js
@@ -7,35 +7,6 @@ var main = function() {
   $.getJSON( url, function( data ) {
     $('.gif').attr('src', data.data.image_url);
   });
-
-
-  var theText = "this is a test";
-
-function nextSize(i,incMethod,textLength)
-{
-  if (incMethod == 1) return (72*Math.abs( Math.sin(i/(textLength/3.14))) );
-  if (incMethod == 2) return (255*Math.abs( Math.cos(i/(textLength/3.14))));
-}
-
-function sizeCycle(text,method,dis)
-{
-  output = "";
-  for (i = 0; i < text.length; i++)
-  {
-    size = parseInt(nextSize(i +dis,method,text.length));
-    output += "<font style='font-size: "+ size +"pt'>" +text.substring(i,i+1)+ "</font>";
-  }
-  theDiv.innerHTML = output;
-}
-
-function doWave(n)
-{  
-  sizeCycle(theText,1,n);
-  if (n > theText.length) {n=0}
-  setTimeout("doWave(" + (n+1) + ")", 50);
-}
-
-doWave(0);
 };
 
 $(document).ready(main);
